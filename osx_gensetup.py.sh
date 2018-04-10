3ecfe545d70ec5ec5378a4c2e7f79775c1733af6
J. Buencuerpo <jbuencuerpo@users.noreply.github.com>
2018-04-09 18:58:23 -0600

    Added OSX makefiles tules for gensetup.py and fix syntax error for clang in S4/S4.cpp

---
+++ osx_gensetup.py.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+OBJDIR="$1"
+LIBFILE="$2"
+LIBS="$3"
+
+cat <<SETUPPY > setup.py
+from distutils.core import setup, Extension
+
+libs = ['S4', 'stdc++']
+libs.extend( [lib[2::] for lib in '$LIBS'.split()])
+
+S4module = Extension('S4',
+	sources = [
+		'S4/main_python.c'
+	],
+	libraries = libs,
+	library_dirs = ['$OBJDIR'],
+	extra_link_args = [
+		'$LIBFILE',
+        '-Wl,-framework',
+        '-Wl,Accelerate',
+    
+	],
+	extra_compile_args=['-std=gnu99'] 
+)
+
+setup(name = 'S4',
+	version = '1.1',
+	description = 'Stanford Stratified Structure Solver (S4): Fourier Modal Method',
+	ext_modules = [S4module]
+)
+SETUPPY
